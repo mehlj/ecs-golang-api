7fb1f6903939328c28e92c58e8e2bde2b70f7e33
Justen Mehl <justen.t.mehl.civ@us.navy.mil>
2023-03-17 16:26:05 -0400

    format map of strings

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,6 @@ variable "pg_dsn" {
 
 variable "image_tag" {
   description = "Image tag that allows terraform to update ECS task definition every pipeline run"
-  type        = string
-  default     = "placeholder"
+  type        = map(string)
+  default     = ["placeholder"]
 }
